aaa515b5c61e6a3b1cf94f9e90a9525b99212723
Myryitzy <104706728+Itzelmariana@users.noreply.github.com>
2022-07-23 22:05:22 -0500

    deleted unused files

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,11 +1,9 @@
 const express = require("express");
 
-const dbRouter = require("./db");
 const notesRouter = require("./notes");
 
 const app = express();
 
-app.use("/db", dbRouter);
 app.use("/notes", notesRouter);
 
 module.exports = app;
